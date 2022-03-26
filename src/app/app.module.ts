f4b5323a9b44db9fb7a4957e939a120ceda6434b
Jonathan Brant <jbrant1995@gmail.com>
2022-03-26 17:21:52 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './compornents/header/header.component';
+import { ContentComponent } from './compornents/content/content.component';
+import { BlogComponent } from './compornents/blog/blog.component';
+import { ArticleComponent } from './compornents/article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    BlogComponent,
+    ArticleComponent,
   ],
   imports: [
     BrowserModule,
